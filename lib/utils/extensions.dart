03bec7c89913afb1f45f3fa25cdd16bc8220acbe
igordev-der <igor.d86@gmail.com>
2020-11-26 05:42:13 +0100

    Adding numberOfTextLinesToDisplay String extension

---
+++ lib/utils/extensions.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/painting.dart';
+
+///Summarizes various String Extensions
+extension TextHelpers on String {
+  ///Returns number of Lines needed to paint whole text on the screen.
+  ///
+  /// Method is usable to understand if string will fit into Text.maxLines
+  /// [style] - text style used for text painting
+  /// [maxWidth] - maximum width of the area for text painting
+  int numberOfTextLinesToDisplay({TextStyle style, double maxWidth}) {
+    final textSpan = TextSpan(text: this, style: style);
+    final textPainter = TextPainter(text: textSpan);
+    textPainter.layout(maxWidth: maxWidth ?? double.infinity);
+    final lineMetrics = textPainter.computeLineMetrics();
+    return lineMetrics.length;
+  }
+}
