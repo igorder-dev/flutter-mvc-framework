5371e4fd276e8d19be77f822318e27c27505fccc
igordev-der <igor.d86@gmail.com>
2021-01-02 11:37:44 +0100

    Async_lock and command pattern

---
+++ lib/utils/async/async_lock.dart
@@ -0,0 +1,11 @@
+import 'dart:async';
+
+class AsyncLock {
+  final Completer _completer = Completer();
+  Future get lock => _completer.future;
+  void release() {
+    _completer.complete();
+  }
+
+  Future call() => lock;
+}
