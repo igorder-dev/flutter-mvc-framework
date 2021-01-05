dd5763a390406dba922b5801ff1c8f77340bca88
igordev-der <igor.d86@gmail.com>
2021-01-05 23:56:42 +0100

    MvcCommand Manager

---
+++ lib/utils/command/MvcCommandManager.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/foundation.dart';
+import 'package:id_mvc_app_framework/framework.dart';
+import 'MvcCommand.dart';
+
+const int EXEC_TRIAL_INTERVAL = 10;
+
+class MvcCommandManager with GetxServiceMixin {
+  factory MvcCommandManager() => Get.put(MvcCommandManager._());
+  MvcCommandManager._();
+  final Map<String, CmdRunner> _workflows = Map();
+
+  final Map<String, CmdRunner> _currentRunner = Map();
+
+  CmdRunner wf<TState extends MvcCmdWorkflowState>(
+      String name, TState initState) {
+    if (!_workflows.containsKey(name)) {
+      _workflows[name] = WorkflowEntryPoint<TState>(initState);
+      initState._workflow = name;
+    }
+    return _workflows[name];
+  }
+}
+
+abstract class CmdRunner<TState extends MvcCmdWorkflowState> {
+  @protected
+  @mustCallSuper
+  Future<void> execute(TState workflowState) async {
+    MvcCommandManager()._currentRunner[workflowState._workflow] = this;
+  }
+}
+
+class SingleCmd<TState extends MvcCmdWorkflowState> extends CmdRunner<TState> {
+  final MvcCommand cmd;
+  final CmdRunner<TState> after;
+  final MvcCommandParams params;
+
+  SingleCmd({
+    this.cmd,
+    this.after,
+    this.params,
+  }) : assert(cmd != null);
+
+  @override
+  Future<void> execute(TState workflowState) async {
+    super.execute(workflowState);
+    await Future.doWhile(() async {
+      await Future.delayed(EXEC_TRIAL_INTERVAL.milliseconds);
+      return !cmd.canExecute;
+    });
+    workflowState.lastCommandResult = await cmd.executeWithFuture(params);
+    if (after != null) after.execute(workflowState);
+  }
+}
+
+class WorkflowEntryPoint<TState extends MvcCmdWorkflowState>
+    extends CmdRunner<TState> {
+  final TState workflowState;
+  CmdRunner<TState> cmd;
+
+  WorkflowEntryPoint(this.workflowState);
+
+  void exec(CmdRunner cmd) {
+    this.cmd = cmd;
+  }
+
+  @override
+  Future<void> execute(TState workflowState) async {
+    assert(cmd != null, "Make sure you start workflow with [exec] command.");
+    super.execute(workflowState);
+    await cmd.execute(workflowState);
+  }
+}
+
+abstract class MvcCmdWorkflowState {
+  String _workflow;
+  MvcCommandResult lastCommandResult;
+}
